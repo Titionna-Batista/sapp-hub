1de5d213c8ad2e28f075e78437f4915fcfcaf6ef
Titionna-Batista <battit@knights.ucf.edu>
2022-12-18 02:08:57 -0800

    Added svgs and updated modal test page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ element.style.backgroundColor = "red";
 element.classList.add("halftone");
 }
 
+function showModal() {
+var modal = document.getElementsByClassName("modal no-display");
+// Going to be a simple show/don't show, work on after styling
+modal.classList.remove(no-display);
+modal.classList.add("display");
+
+}
+
 function hideChar() {
    var element = document.getElementsByClassName("style-container")[0];
    var image = document.getElementById("lluxim")
